d7fb359640ed29e15aece4ca03bf1b1e64cfbad6
Daniel Schneider <daschne8@gmail.com>
2019-01-28 04:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  itemInCart = false;
+  var itemInCart = false;
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i,1);
