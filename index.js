d4b345bf96278a6ecfd55af1944a32eea6b76adc
Daniel Schneider <daschne8@gmail.com>
2019-01-28 04:20:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber === null){
+  if(cardNumber === ""){
     return "Sorry, we don't have a credit card on file for you."
   }
   cart = [];
